f5165a96921ed1c083b612b7820c1196a3c37390
sukrit sangvong <sukritsangvong@eduroam-163-3.dyn.carleton.edu>
2019-11-09 20:22:23 -0600

    calculateScore done

--- calculateScore.py
+++ calculateScore.py
@@ -1,13 +1,26 @@
 def calculateScore(listOfGraveyard):
+    pointList = []
+    QofSpades = False
+    
+    for card in listOfGraveyard:
+        
+        if card[1] == 'hearts':
+            pointList += card
+        if card[1] == 'spades' and card[0] == 'Q':
+            QofSpades = True
 
+    points = int(len(pointList)/2)
+    if QofSpades:
+        points += 13
 
+    return points
 
 
 
 
 
 def main():
-    calculateScore()
+    print(calculateScore([['8', 'hearts'],['2', 'clubs'],['9', 'hearts'],['5', 'clubs'],['Q', 'spades']]))
 
 
 if __name__ == "__main__":
